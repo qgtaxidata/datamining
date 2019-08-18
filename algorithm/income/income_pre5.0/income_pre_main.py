c29c2482de9298400de8c7880d98794d12f924b9
hahahahahzt <1284629360@qq.com>
2019-08-18 16:19:17 +0800

    income_pre5.0

---
+++ algorithm/income/income_pre5.0/income_pre_main.py
@@ -0,0 +1,1 @@
+import numpy as npimport pandas as pdimport Pre_income_ARIMRimport Pre_income_ARMRdef main_pre(date,area):    try:        pre_result = Pre_income_ARIMR.main_ARIMR(date,area)    except:        pre_result = Pre_income_ARMR.main_ARMR(date, area)if __name__ == '__main__':    main_pre('2017-02-15',11)          # 注：由于第四区：从化区收入量过少，预测结果都是0。
