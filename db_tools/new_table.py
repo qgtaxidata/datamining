a530b27b4b7333e24bbce016e4789fa3ea4c0150
vvlj <1921749915@qq.com>
2019-08-19 09:42:33 +0800

    update map_tools and db_tools

--- db_tools/new_table.py
+++ db_tools/new_table.py
@@ -50,19 +50,38 @@ def new_muti_operate_params(table_itr=range(3, 5),table_base='operate_his',class
     session.close()
     _session.close()
     return True
-# class _Operate(BaseOperate,_Base):
-#     __tablename__ = 'operate_his1'
-# def new_operate():
-#     _session = _Session()
-#     session = Session()
-#
-#     for col in _session.query(_Operate).filter(_Operate.GET_ON_LATITUDE != 0).filter(_Operate.GET_ON_LATITUDE != 0).all():
-#         col = Operate(ID=col.ID,WORK_BEGIN_TIME=col.WORK_BEGIN_TIME,GET_ON_LONGITUDE=col.GET_ON_LONGITUDE,
-#                        GET_ON_LATITUDE=col.GET_ON_LATITUDE,GEOHASH5=col.GEOHASH5,GEOHASH7=col.GEOHASH7)
-#         session.add(col)
-#     session.commit()
-#     return True
+
+def new_zone_quality():
+    from settings import g_boundury,g_name
+    drop_table(ZoneQuality)
+    s = Session()
+    d_match = {}
+    rst = s.query(Quality.rid,Quality.hour_repre,Quality.average_time, Quality.density, Quality.flow).all()
+    for rid,h,a_time,density,flow in rst:
+        ghash5, = s.query(Records.geohash5).filter(Records.id==rid).first()
+        for i,b in enumerate(g_boundury):
+            if ghash5 in b:
+                if i in d_match:
+                    match = d_match[i]
+                    if h in match:
+                        match[h]['a_time'] = (match[h]['a_time'] + a_time) / 2
+                        match[h]['density'] = (match[h]['density'] + density) / 2
+                        match[h]['flow'] = (match[h]['flow'] + flow) / 2
+                    else:
+                        match[h] = {'a_time': a_time, 'density': density, 'flow': flow}
+                else:
+                    d_match[i] = {h: {'a_time': a_time, 'density': density, 'flow': flow}}
+                break
+    for i,match in d_match.items():
+        for h in match:
+            a_time = match[h]['a_time']
+            density = match[h]['density']
+            flow = match[h]['flow']
+            s.add(ZoneQuality(g_zone=i,g_name=g_name[i],hour_repre=str(h),average_time=a_time,density=density,flow=flow))
+    s.commit()
+    s.close()
 
 if __name__ =='__main__':
-    new_muti_operate((1,))    #或者(3,4,5)，传入一个可迭代对象
+    #new_muti_operate((1,))    #或者(3,4,5)，传入一个可迭代对象
     #new_muti_operate_params((1,))  # 或者(3,4,5)，传入一个可迭代对象
+    new_zone_quality()
